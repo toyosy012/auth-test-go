f1c7e1aa647196648e08f42f0b13f56280f4b011
yasui <s.yasui@accelia.net>
2021-07-22 19:42:58 +0900

    feat: AUTH-TEST-#1 環境変数の取得モデル
    環境変数クラス追加

---
+++ infra/environment.go
@@ -0,0 +1,12 @@
+package infra
+
+type Environment struct {
+	Email        string `envconfig:"EMAIL" required:"true"`
+	UserPassword string `envconfig:"USER_PASSWORD" required:"true"`
+	UserName     string `envconfig:"USER_NAME" required:"true"`
+	User         string `default:"root"`
+	Password     string `default:"mysql"`
+	Host         string `default:"0.0.0.0"`
+	Port         int    `default:"3306"`
+	Name         string `default:"auth_test"`
+}
