728fb94de2b559cde5013a31c623e511c1e43e4d
toyosy012 <toyosy012@gmail.com>
2023-01-29 09:59:45 +0900

    ISSUE-#26: refactor/fix_names ルーティングパス修正
    各定義の`/`を削除
    
    ユーザ作成APIをバージョングループの外に移動

--- main.go
+++ main.go
@@ -40,35 +40,37 @@ func main() {
 	jwtAuth := controller.NewJWTAuth(authSvc)
 
 	router := gin.Default()
-	router.GET("/users/:id", userAccountController.Get)
-
+	usersRouter := router.Group("users")
+	{
+		usersRouter.GET(":id", userAccountController.Get)
+		usersRouter.POST("new", userAccountController.Create)
+	}
 	userSessionRepo := db.NewUserSessionRepo(*dbClient)
 	storedAuthSvc := services.NewStoredAuthorization(userAccountRepo, userSessionRepo, env.AvailabilityTime)
 	storedAuth := controller.NewStoredAuth(storedAuthSvc)
-	v0 := router.Group("/v0")
+	v0 := router.Group("v0")
 	{
-		v0.POST("/login", storedAuth.Login)
+		v0.POST("login", storedAuth.Login)
 
-		v0usersRouter := v0.Group("/users")
+		v0UsersRouter := v0.Group("users")
 		{
-			v0usersRouter.POST("/new", userAccountController.Create)
-			v0usersRouter.Use(storedAuth.CheckAuthentication).GET("", userAccountController.List)
-			authedOwnerRouter := v0usersRouter.Use(storedAuth.CheckAuthenticatedOwner)
+			v0UsersRouter.Use(storedAuth.CheckAuthentication).GET("", userAccountController.List)
+			authedOwnerRouter := v0UsersRouter.Use(storedAuth.CheckAuthenticatedOwner)
 			{
-				authedOwnerRouter.PATCH("/:id", userAccountController.Update)
+				authedOwnerRouter.PATCH(":id", userAccountController.Update)
 				authedOwnerRouter.Use(storedAuth.CheckAuthenticatedOwner).
-					DELETE("/:id", userAccountController.Delete)
+					DELETE(":id", userAccountController.Delete)
 				authedOwnerRouter.Use(storedAuth.CheckAuthenticatedOwner).
-					DELETE("/logout/:id", storedAuth.Logout)
+					DELETE("logout/:id", storedAuth.Logout)
 			}
 		}
 	}
 
-	v1 := router.Group("/v1")
+	v1 := router.Group("v1")
 	{
-		v1.POST("/login", jwtAuth.Login)
+		v1.POST("login", jwtAuth.Login)
 
-		authRouter := v1.Group("/users").Use(jwtAuth.CheckAuthentication)
+		authRouter := v1.Group("users").Use(jwtAuth.CheckAuthentication)
 		{
 			authRouter.PATCH(":id", userAccountController.Update)
 			authRouter.DELETE(":id", userAccountController.Delete)
