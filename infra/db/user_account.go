ef9e8fa43a825a99706bed41e9de0282a52831db
toyosy012 <toyosy012@gmail.com>
2023-02-05 20:30:14 +0900

    ISSUE-#24: refactor/fix_user_account_error_handling ユーザアカウントエラーハンドリング修正
    ユーザアカウント処理でエラーをラップする処理を追加
    
    DB処理のエラーをラップして返却するように修正
    パスワードの暗号化エラーをラップ
    レコードが存在しない場合のエラーをラップ
    ユーザEmailの重複エラーをラップ
    UUIDのパースエラーをラップ

--- infra/db/user_account.go
+++ infra/db/user_account.go
@@ -1,10 +1,15 @@
 package db
 
 import (
+	"errors"
+	"fmt"
+
+	"github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 
 	"auth-test/models"
+	"auth-test/services"
 )
 
 type UserAccounts struct {
@@ -38,7 +43,12 @@ func (r *UserAccountRepository) Find(id string) (*models.UserAccount, error) {
 	var account UserAccounts
 	result := r.mysql.Where("id=?", id).First(&account)
 	if err := result.Error; err != nil {
-		return nil, err
+		switch {
+		case errors.Is(err, gorm.ErrRecordNotFound):
+			return nil, services.NewApplicationErr(services.NoUserRecord, err)
+		default:
+			return nil, services.NewApplicationErr(services.InternalServerErr, err)
+		}
 	}
 
 	response := models.NewUserAccount(account.ID, account.Email, account.Name, account.Hash)
@@ -49,7 +59,12 @@ func (r *UserAccountRepository) FindByEmail(email string) (*models.UserAccount, 
 	var account UserAccounts
 	result := r.mysql.Where("email=?", email).First(&account)
 	if err := result.Error; err != nil {
-		return nil, err
+		switch {
+		case errors.Is(err, gorm.ErrRecordNotFound):
+			return nil, services.NewApplicationErr(services.NoUserEmail, err)
+		default:
+			return nil, services.NewApplicationErr(services.InternalServerErr, err)
+		}
 	}
 
 	response := models.NewUserAccount(account.ID, account.Email, account.Name, account.Hash)
@@ -60,7 +75,7 @@ func (r *UserAccountRepository) List() ([]models.UserAccount, error) {
 	var accounts []UserAccounts
 	result := r.mysql.Find(&accounts)
 	if err := result.Error; err != nil {
-		return nil, err
+		return nil, services.NewApplicationErr(services.NoUsersRecord, err)
 	}
 	var results []models.UserAccount
 	for _, account := range accounts {
@@ -74,19 +89,29 @@ func (r *UserAccountRepository) List() ([]models.UserAccount, error) {
 func (r *UserAccountRepository) Insert(id, email, name, password string) (*models.UserAccount, error) {
 	encryptedPass, err := models.NewEncryption(password)
 	if err != nil {
-		return nil, err
+		return nil, services.NewApplicationErr(services.TooLongPassword, err)
 	}
 	account := NewUserAccount(id, email, name, encryptedPass.Hash())
 
 	result := r.mysql.Create(account)
 	if err = result.Error; err != nil {
-		return nil, err
+		switch {
+		case err.(*mysql.MySQLError).Number == MySQLDuplicateEntry:
+			return nil, services.NewApplicationErr(services.DuplicateUserEmail, err)
+		default:
+			return nil, services.NewApplicationErr(services.InternalServerErr, err)
+		}
 	}
 
 	var a UserAccounts
 	result = r.mysql.Where("email = ?", email).Find(&a)
-	if result.Error != nil {
-		return nil, result.Error
+	if err = result.Error; err != nil {
+		switch {
+		case errors.Is(err, gorm.ErrRecordNotFound):
+			return nil, services.NewApplicationErr(services.NoUserRecord, err)
+		default:
+			return nil, services.NewApplicationErr(services.InternalServerErr, err)
+		}
 	}
 
 	response := models.NewUserAccount(a.ID, a.Email, a.Name, a.Hash)
@@ -96,19 +121,21 @@ func (r *UserAccountRepository) Insert(id, email, name, password string) (*model
 func (r *UserAccountRepository) Update(account models.UserAccount) (*models.UserAccount, error) {
 	encryptedPass, err := models.NewEncryption(account.Password())
 	if err != nil {
-		return nil, err
+		return nil, services.NewApplicationErr(services.TooLongPassword, err)
 	}
 
 	newAccount := UserAccounts{Email: account.Email(), Name: account.Name(), Hash: encryptedPass.Hash()}
-	result := r.mysql.Table("user_accounts").Where("id=?", account.ID()).UpdateColumns(newAccount)
-	if err = result.Error; err != nil {
-		return nil, err
-	}
-
 	var a UserAccounts
-	result = r.mysql.Where("email = ?", account.Email()).Find(&a)
-	if result.Error != nil {
-		return nil, result.Error
+	result := r.mysql.Table("user_accounts").Where("id = ?", account.ID()).UpdateColumns(newAccount).First(&a)
+	if err = result.Error; err != nil {
+		switch {
+		case errors.Is(err, gorm.ErrRecordNotFound):
+			return nil, services.NewApplicationErr(services.NoUserRecord, err)
+		case err.(*mysql.MySQLError).Number == MySQLDuplicateEntry:
+			return nil, services.NewApplicationErr(services.DuplicateUserEmail, err)
+		default:
+			return nil, services.NewApplicationErr(services.InternalServerErr, err)
+		}
 	}
 
 	response := models.NewUserAccount(a.ID, a.Email, a.Name, a.Hash)
@@ -118,12 +145,14 @@ func (r *UserAccountRepository) Update(account models.UserAccount) (*models.User
 func (r *UserAccountRepository) Delete(id string) error {
 	deletedUUID, err := uuid.Parse(id)
 	if err != nil {
-		return err
+		return services.NewApplicationErr(services.InvalidUUIDFormat, err)
 	}
-	var user models.UserAccount
-	result := r.mysql.Where("id = ?", deletedUUID).Delete(&user)
-	if result.Error != nil {
-		return result.Error
+
+	result := r.mysql.Delete(&UserAccounts{}, deletedUUID)
+	if result.RowsAffected == NoDeleteRecords {
+		return services.NewApplicationErr(services.NoUserRecord, fmt.Errorf("削除対象ID: %s", id))
+	} else if result.Error != nil {
+		return services.NewApplicationErr(services.InternalServerErr, result.Error)
 	}
 	return nil
 }
